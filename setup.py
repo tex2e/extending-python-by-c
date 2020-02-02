0759ffe1c8fef1accc4d4348ff083f831549212a
Mako <mnfeconicu41@gmail.com>
2020-02-02 16:34:21 +0900

    update setup.py to compile xor.c

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('spam',
-                    sources = ['spammodule.c'])
+                    sources = ['spam.c'])
+module2 = Extension('xor',
+                    sources = ['xor.c'],
+                    extra_compile_args = ['-O3'])
 
-setup (name = 'Spam',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
+setup(name = 'Spam',
+      version = '1.0',
+      description = 'This is a demo package',
+      ext_modules = [module1, module2])
