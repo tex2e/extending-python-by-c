d85758197143402f75bfd3af67dbabdc5f916d0f
Mako <mnfeconicu41@gmail.com>
2020-02-02 16:33:45 +0900

    rename spammodule -> spam

--- spam.c
+++ spam.c
@@ -8,31 +8,32 @@ spam_system(PyObject *self, PyObject *args)
     const char *command;
     int sts;
 
-    if (!PyArg_ParseTuple(args, "s", &command)) // 文字列を1つ受け取る
+    // 文字列を1つ受け取る
+    if (!PyArg_ParseTuple(args, "s", &command))
         return NULL;
     sts = system(command); // シェルを実行する
-    return PyLong_FromLong(sts); // Pythonオブジェクトに変換して返す
+    return PyLong_FromLong(sts); // 結果をPythonオブジェクトに変換して返す
 }
 
 // メソッドテーブル
-static PyMethodDef SpamMethods[] = {
+static PyMethodDef MethodTable[] = {
     // {関数名, アドレス, 引数, ドキュメント}
     {"system", spam_system, METH_VARARGS,
      "Execute a shell command."},
     {NULL, NULL, 0, NULL} // 番兵
 };
 
-static struct PyModuleDef spammodule = {
+static struct PyModuleDef module = {
     PyModuleDef_HEAD_INIT,
     "spam",     // モジュール名
     NULL,       // モジュールのドキュメント
     -1,         // モジュールがグローバル変数に状態を保持する場合は -1
-    SpamMethods // モジュールのメソッドテーブル
+    MethodTable // モジュールのメソッドテーブル
 };
 
-// 初期化関数
+// モジュール初期化関数
 PyMODINIT_FUNC
 PyInit_spam(void)
 {
-    return PyModule_Create(&spammodule);
+    return PyModule_Create(&module);
 }
